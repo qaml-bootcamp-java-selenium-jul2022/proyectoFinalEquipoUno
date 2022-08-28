e0f21a67cba182eda6e044b30d84e647647eafc6
Alvaro Meraz <alvmmn@gmail.com>
2022-08-27 22:33:55 -0500

    Agrego Asserts de los datos del cliente en RentaTest y VentaTest

--- src/test/java/EjerciciosProyectoFinal/RentaTest.java
+++ src/test/java/EjerciciosProyectoFinal/RentaTest.java
@@ -7,17 +7,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RentaTest {
-    Renta rentaTest = new Renta("NombreTest", "direccion de Prueba", "3344556677", true, "correoTest@gmail.com", PagoElectronico.TRANSFERENCIA);
+    Renta rentaTest = new Renta("NombreRentaTest", "direccion de Prueba", "3344556677", true, "correoTest@gmail.com", PagoElectronico.TRANSFERENCIA);
 
-    Prenda pantalon = new Pantalon("Pantalon", EstadosDeMexico.JALISCO, CategoriaPrenda.PRENDA_COMUN, 1000, 200, 1500);
-    Prenda sombrero = new Sombrero("Sombrero", EstadosDeMexico.NUEVO_LEON, CategoriaPrenda.PRENDA_GALA, 1000, 200, 1500);
+    Prenda pantalon = new Pantalon("Pantalon", EstadosDeMexico.JALISCO, CategoriaPrenda.PRENDA_COMUN, 1000, 500, 2000);
+    Prenda sombrero = new Sombrero("Sombrero", EstadosDeMexico.NUEVO_LEON, CategoriaPrenda.PRENDA_GALA, 1000, 500, 2000);
+    Prenda blusa = new Blusa("Blusa", EstadosDeMexico.JALISCO, CategoriaPrenda.PRENDA_COMUN, 1000, 500, 2000);
+    Prenda falda = new Falda("Falda", EstadosDeMexico.NUEVO_LEON, CategoriaPrenda.PRENDA_GALA, 1000, 500, 2000);
+    Prenda zapato = new Zapato("Zapato", EstadosDeMexico.JALISCO, CategoriaPrenda.PRENDA_COMUN, 1000, 500, 2000);
+
 
     @Test
     void testRenta() {
-        String expectedClienteNombre = "NombreTest";
-        Integer expectedTotalRenta = 400;
+        String expectedClienteNombre = "NombreRentaTest";
+        String expectedDireccionCliente = "direccion de Prueba";
+        String expectedTelefono="3344556677";
+        Boolean expectedClienteRegistrado = true;
+        String expectedCorreoCliente = "correoTest@gmail.com";
+        PagoElectronico expectedPagoElectronico = PagoElectronico.TRANSFERENCIA;
+
+        Integer expectedTotalRenta = 2500;
         rentaTest.prendasEnRenta.add(pantalon);
         rentaTest.prendasEnRenta.add(sombrero);
+        rentaTest.prendasEnRenta.add(blusa);
+        rentaTest.prendasEnRenta.add(falda);
+        rentaTest.prendasEnRenta.add(zapato);
 
         for (Prenda prendaEnRenta : rentaTest.prendasEnRenta) {
             rentaTest.totalCostoRenta = rentaTest.totalCostoRenta + prendaEnRenta.costoRenta;
@@ -25,5 +38,11 @@ class RentaTest {
 
         Assert.assertEquals(expectedClienteNombre,rentaTest.cliente.getNombreCliente());
         Assert.assertEquals(expectedTotalRenta, rentaTest.totalCostoRenta);
+        Assert.assertEquals(expectedDireccionCliente,rentaTest.cliente.getDireccionCliente());
+        Assert.assertEquals(expectedTelefono,rentaTest.cliente.getTelefonoCliente());
+        Assert.assertEquals(expectedClienteRegistrado,rentaTest.cliente.isClienteRegistrado());
+        Assert.assertEquals(expectedCorreoCliente, rentaTest.cliente.getCorreoElectronico());
+        Assert.assertEquals(expectedPagoElectronico,rentaTest.cliente.getPagoElectronico());
+
     }
 }
--- src/test/java/EjerciciosProyectoFinal/VentaTest.java
+++ src/test/java/EjerciciosProyectoFinal/VentaTest.java
@@ -8,15 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class VentaTest {
     Venta ventaTest = new Venta("NombreVentaTest", "direccion de Prueba venta", "3344556677", true, "correoTest@amail.com", PagoElectronico.TRANSFERENCIA);
 
-    Prenda zapato1 = new Zapato("Zapato", EstadosDeMexico.SINALOA, CategoriaPrenda.PRENDA_COMUN, 1000, 200, 2500);
-    Prenda blusa1 = new Blusa("Blusa", EstadosDeMexico.NAYARIT, CategoriaPrenda.PRENDA_GALA, 1000, 200, 2500);
+    Prenda zapato = new Zapato("Zapato", EstadosDeMexico.SINALOA, CategoriaPrenda.PRENDA_COMUN, 1000, 200, 2500);
+    Prenda blusa = new Blusa("Blusa", EstadosDeMexico.NAYARIT, CategoriaPrenda.PRENDA_GALA, 1000, 200, 2500);
+    Prenda pantalon = new Pantalon("Pantalon", EstadosDeMexico.JALISCO, CategoriaPrenda.PRENDA_COMUN, 1000, 500, 2500);
+    Prenda sombrero = new Sombrero("Sombrero", EstadosDeMexico.NUEVO_LEON, CategoriaPrenda.PRENDA_GALA, 1000, 500, 2500);
+    Prenda falda = new Falda("Falda", EstadosDeMexico.NUEVO_LEON, CategoriaPrenda.PRENDA_GALA, 1000, 500, 5000);
+
 
     @Test
     void testVenta() {
         String expectedClienteNombre = "NombreVentaTest";
-        Integer expectedTotalVenta = 5000;
-        ventaTest.prendasEnVenta.add(zapato1);
-        ventaTest.prendasEnVenta.add(blusa1);
+        String expectedDireccionCliente = "direccion de Prueba venta";
+        String expectedTelefono="3344556677";
+        Boolean expectedClienteRegistrado = true;
+        String expectedCorreoCliente = "correoTest@amail.com";
+        PagoElectronico expectedPagoElectronico = PagoElectronico.TRANSFERENCIA;
+        Integer expectedTotalVenta = 15000;
+        ventaTest.prendasEnVenta.add(zapato);
+        ventaTest.prendasEnVenta.add(blusa);
+        ventaTest.prendasEnVenta.add(pantalon);
+        ventaTest.prendasEnVenta.add(sombrero);
+        ventaTest.prendasEnVenta.add(falda);
 
         for (Prenda prendaEnRenta : ventaTest.prendasEnVenta) {
             ventaTest.totalCostoVenta = ventaTest.totalCostoVenta + prendaEnRenta.costoVenta;
@@ -24,5 +36,10 @@ class VentaTest {
 
         Assert.assertEquals(expectedClienteNombre,ventaTest.cliente.getNombreCliente());
         Assert.assertEquals(expectedTotalVenta, ventaTest.totalCostoVenta);
+        Assert.assertEquals(expectedDireccionCliente,ventaTest.cliente.getDireccionCliente());
+        Assert.assertEquals(expectedTelefono,ventaTest.cliente.getTelefonoCliente());
+        Assert.assertEquals(expectedClienteRegistrado,ventaTest.cliente.isClienteRegistrado());
+        Assert.assertEquals(expectedCorreoCliente, ventaTest.cliente.getCorreoElectronico());
+        Assert.assertEquals(expectedPagoElectronico,ventaTest.cliente.getPagoElectronico());
     }
 }
